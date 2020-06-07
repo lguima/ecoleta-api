9c2b50e6e6098bbf7d83a97baaa3282d766ccd22
Lucas Guima <lucasguima.felipe@gmail.com>
2020-06-07 16:15:06 -0300

    Update build files

--- build/src/server.js
+++ build/src/server.js
@@ -14,4 +14,4 @@ app.use(express_1.default.json());
 app.use(routes_1.default);
 app.use('/uploads', express_1.default.static(path_1.default.resolve(__dirname, '..', 'uploads')));
 app.use(celebrate_1.errors());
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
