a6ca01cf9372021f369231a2ef6c7ee7b47efe95
Lucas Guima <lucasguima.felipe@gmail.com>
2020-06-11 19:38:34 -0300

    Remove build files

--- build/src/server.js
+++ build/src/server.js
@@ -9,7 +9,10 @@ var routes_1 = __importDefault(require("./routes"));
 var path_1 = __importDefault(require("path"));
 var celebrate_1 = require("celebrate");
 var app = express_1.default();
-app.use(cors_1.default());
+app.use(cors_1.default({
+    origin: process.env.ECOLETA_WEB_URL,
+    optionsSuccessStatus: 200,
+}));
 app.use(express_1.default.json());
 app.use(routes_1.default);
 app.use('/uploads', express_1.default.static(path_1.default.resolve(__dirname, '..', 'uploads')));
